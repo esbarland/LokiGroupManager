2bf8fbaa53be5ab7ad6e34229e0efb57f2a577db
wigarrier <william.garrier@etu.uca.fr>
2019-02-28 16:33:54 +0100

    Ajout d'user OK

--- Code/LokiGroupManager/app/src/main/java/com/example/lokigroupmanager/BasicsActivity/UsersActivity.java
+++ Code/LokiGroupManager/app/src/main/java/com/example/lokigroupmanager/BasicsActivity/UsersActivity.java
@@ -2,7 +2,9 @@ package com.example.lokigroupmanager.BasicsActivity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -16,16 +18,18 @@ import com.example.lokigroupmanager.R;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UsersActivity extends AppCompatActivity{
+public class UsersActivity extends AppCompatActivity implements AddUserDialog.AddUserDialogListener {
 
     private ListView listViewUsers;
+    private List<User> listAllUsers;
+    private ArrayAdapter<User> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.add);
+        FloatingActionButton fab = findViewById(R.id.add);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,24 +38,34 @@ public class UsersActivity extends AppCompatActivity{
             }
         });
 
-        listViewUsers = (ListView) findViewById(R.id.listUsers);
+        listViewUsers = findViewById(R.id.listUsers);
 
         StubDataManager stub = new StubDataManager();
 
         List<Group> listGroups = stub.loadGroups();
 
-        List<User> listAllUsers = new ArrayList<>();
+        listAllUsers = new ArrayList<>();
 
         for (Group group : listGroups) {
             listAllUsers.addAll(group.getListUsers());
         }
 
-        ArrayAdapter<User> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listAllUsers);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listAllUsers);
 
         listViewUsers.setAdapter(adapter);
-
     }
 
-
-
+    /***
+     * Add user bundle reception
+     * @param bundle user's informations to add
+     *               firstname, surname, email as keys.
+     */
+    @Override
+    public void onFinishAddDialog(Bundle bundle) {
+        if(bundle != null){
+            User u = new User(bundle.getString("firstname"), bundle.getString("surname"), bundle.getString("email"));
+            listAllUsers.add(u);
+            adapter.notifyDataSetChanged();
+        }
+    }
 }
--- Code/LokiGroupManager/app/src/main/java/com/example/lokigroupmanager/Dialogs/AddUserDialog.java
+++ Code/LokiGroupManager/app/src/main/java/com/example/lokigroupmanager/Dialogs/AddUserDialog.java
@@ -4,18 +4,27 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Toolbar;
 
+import com.example.lokigroupmanager.BasicsActivity.UsersActivity;
 import com.example.lokigroupmanager.R;
 
 public class AddUserDialog extends DialogFragment {
     public static String TAG = "FullScreenDialog";
+
+    // Interface to send and receive data from the dialog to the activity
+    public interface AddUserDialogListener {
+        void onFinishAddDialog(Bundle bundle);
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,29 +36,62 @@ public class AddUserDialog extends DialogFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        View v = inflater.inflate(R.layout.add_user_dialog, container, false);
-
-        ImageButton close = (ImageButton) v.findViewById(R.id.add_user_close);
-
-        // Close button behavior
-        close.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getDialog().dismiss();
-            }
-        });
-
-        return v;
+        final View view = inflater.inflate(R.layout.add_user_dialog, container, false);
+        return view;
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        Dialog dialog = getDialog();
+        final Dialog dialog = getDialog();
         if (dialog != null) {
             int width = ViewGroup.LayoutParams.MATCH_PARENT;
             int height = ViewGroup.LayoutParams.MATCH_PARENT;
             dialog.getWindow().setLayout(width, height);
+
+            // Get the Button components from the view
+            ImageButton close = getView().findViewById(R.id.add_user_close);
+            Button save = getView().findViewById(R.id.add_user_save);
+
+            // Close button behavior
+            close.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    getDialog().dismiss();
+                }
+            });
+
+            // Save button behavior
+            save.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // Get the EditText components from the view.
+                    EditText firstnameEditText = getView().findViewById(R.id.add_user_firstname);
+                    EditText surnameEditText = getView().findViewById(R.id.add_user_surname);
+                    EditText emailEditText = getView().findViewById(R.id.add_user_email);
+
+                    // Get the texts from the EditText components
+                    String firstname = firstnameEditText.getText().toString();
+                    String surname = surnameEditText.getText().toString();
+                    String email = emailEditText.getText().toString();
+
+                    // Values check
+                    if(firstname.equals("") || surname.equals("") || email.equals("")) {
+                        // Error case
+                        Snackbar.make(getView(), getString(R.string.emptyfield), Snackbar.LENGTH_LONG).show();
+                    }
+                    else {
+                        // Send the bundle to the activity
+                        Bundle infos = new Bundle();
+                        infos.putString("firstname", firstname);
+                        infos.putString("surname", surname);
+                        infos.putString("email", email);
+                        AddUserDialogListener listener = (AddUserDialogListener) getActivity();
+                        listener.onFinishAddDialog(infos);
+                        dismiss();
+                    }
+                }
+            });
         }
     }
 }
